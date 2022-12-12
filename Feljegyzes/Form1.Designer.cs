74c69fb950012939e507f42c27d918bcbf819feb
cisco <cisco@B101-16>
2022-12-12 09:47:18 +0100

    Add project files.

---
+++ Feljegyzes/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Feljegyzes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_save = new System.Windows.Forms.Button();
+            this.button_open = new System.Windows.Forms.Button();
+            this.richTextBox_szoveg = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_nev = new System.Windows.Forms.TextBox();
+            this.dateTimePicker_datum = new System.Windows.Forms.DateTimePicker();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(484, 77);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(136, 40);
+            this.button_save.TabIndex = 0;
+            this.button_save.Text = "Mentés";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(484, 158);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(136, 40);
+            this.button_open.TabIndex = 1;
+            this.button_open.Text = "Megnyitás";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // richTextBox_szoveg
+            // 
+            this.richTextBox_szoveg.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox_szoveg.Location = new System.Drawing.Point(0, 267);
+            this.richTextBox_szoveg.Name = "richTextBox_szoveg";
+            this.richTextBox_szoveg.Size = new System.Drawing.Size(800, 183);
+            this.richTextBox_szoveg.TabIndex = 2;
+            this.richTextBox_szoveg.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Név:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dátum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(295, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Feljegyzés";
+            // 
+            // textBox_nev
+            // 
+            this.textBox_nev.Location = new System.Drawing.Point(138, 88);
+            this.textBox_nev.Name = "textBox_nev";
+            this.textBox_nev.Size = new System.Drawing.Size(158, 20);
+            this.textBox_nev.TabIndex = 6;
+            // 
+            // dateTimePicker_datum
+            // 
+            this.dateTimePicker_datum.Location = new System.Drawing.Point(138, 152);
+            this.dateTimePicker_datum.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker_datum.Name = "dateTimePicker_datum";
+            this.dateTimePicker_datum.Size = new System.Drawing.Size(190, 20);
+            this.dateTimePicker_datum.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker_datum);
+            this.Controls.Add(this.textBox_nev);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox_szoveg);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.button_save);
+            this.Name = "Form1";
+            this.Text = "Feljegyzés";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.RichTextBox richTextBox_szoveg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_nev;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_datum;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Feljegyzes/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Feljegyzes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox_nev.Text))
+            {
+                MessageBox.Show("Adjon meg egy nevet!!!!");
+                return;
+            }
+            if (string.IsNullOrEmpty(richTextBox_szoveg.Text))
+            {
+                MessageBox.Show("Nem adott meg szöveget!!!!!");
+                return;
+            }
+            saveFileDialog1.Filter = "Egyszerű szöveg fájl| *.txt| Vesszővel tagolt szövegfájl(*.csv) |*.csv| Minden fájl|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string szov = string.Join(";",textBox_nev.Text, richTextBox_szoveg.Text);
+                string kivalasztottFile = saveFileDialog1.FileName;
+                File.WriteAllText(kivalasztottFile, szov);
+                textBox_nev.Text = "";
+                richTextBox_szoveg.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Nincs kiválasztva!!!!!");
+            }
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string kivFile = openFileDialog1.FileName;
+                string beolvasottSzov = File.ReadAllText(kivFile);
+                string[] adatok = beolvasottSzov.Split(';');
+                textBox_nev.Text = adatok[0];
+                richTextBox_szoveg.Text = adatok[1];
+            }
+        }
+    }
+}
